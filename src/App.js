1f8b009f178ea091e1ca0068809007aea9f37e29
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-11-20 22:07:53 +0100

    add AddBookForm component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useEffect, useState, useRef} from "react";
 import BooksAPI from "./api";
 import Header from "./components/Header";
 import RandomBook from "./components/RandomBook";
+import AddBookForm from "./components/AddBookForm";
 
 const App = () => {
 
@@ -34,70 +35,6 @@ const App = () => {
       setCurrentBooks(currently)})
   },[]);
 
-
-  const titleInput = useRef(null);
-  const authorInput = useRef(null);
-  const coverInput = useRef(null);
-  const radioInput1 = useRef(null);
-  const radioInput2 = useRef(null);
-  const radioInput3 = useRef(null);
-
-
-  const handleAddBook = (book) => {
-    getRadioValue()
-    let base
-    let method
-   if(radioShelf === 'to-read') {
-      base = booksToRead
-      method = setBooksToRead
-    }
-    else if(radioShelf === 'currently-reading') {
-      base = currentBooks
-      method = setCurrentBooks
-    }
-    else if(radioShelf === 'read') {
-      base = booksRead
-      method = setBooksRead
-    }
-    BooksAPI.addBook(book).then(
-        (bookToAdd) => method([...base, bookToAdd])
-    )
-    BooksAPI.addBook(book).then(
-        (bookToAdd) => setBooks([...books, bookToAdd])
-    )
-  }
-
-  let newBook = {}
-  let radioShelf
-
-  const getRadioValue = () => {
-    if (radioInput1.current.checked) {
-      radioShelf = radioInput1.current.value
-    }
-    else if (radioInput2.current.checked) {
-      radioShelf = radioInput2.current.value
-    }
-    else if (radioInput3.current.checked) {
-      radioShelf = radioInput3.current.value
-    }
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    getRadioValue()
-    newBook = {
-      title: titleInput.current.value,
-      author: authorInput.current.value,
-      cover: coverInput.current.value,
-      exclusiveShelf: radioShelf,
-    };
-    handleAddBook(newBook, newBook.exclusiveShelf)
-    console.log(newBook.exclusiveShelf)
-
-    titleInput.current.value = "";
-    authorInput.current.value = "";
-    coverInput.current.value = "";
-  }
 
 
   //refactor to switch, change variables names
@@ -133,15 +70,9 @@ const App = () => {
     <div>
       <Header />
       <RandomBook booksToRead={booksToRead}/>
-      <form onSubmit={handleSubmit}>
-        <label>Tytuł<input type="text" ref={titleInput}/></label>
-        <label>Autor<input type="text" ref={authorInput}/></label>
-        <label>Okładka<input type="text" ref={coverInput}/></label><br/>
-        <label>Przeczytane<input type="radio" name="shelf" value="read" ref={radioInput1}/></label><br/>
-        <label>Do przeczytania<input type="radio" name="shelf" value="to-read" ref={radioInput2}/></label><br/>
-        <label>Aktualnie czytane<input type="radio" name="shelf" value="currently-reading" ref={radioInput3}/></label><br/>
-        <button>DODAJ KSIĄŻKĘ</button>
-      </form>
+      <AddBookForm booksToRead={booksToRead} booksRead={booksRead} currentBooks={currentBooks} books={books}
+                   setBooks={setBooks} setBooksRead={setBooksRead} setBooksToRead={setBooksToRead}
+                   setCurrentBooks={setCurrentBooks}/>
 
       <details><summary>Wszystkie książki</summary>
         {books.map(book => (
