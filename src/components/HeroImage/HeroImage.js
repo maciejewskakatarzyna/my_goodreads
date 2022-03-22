382f5c7cfc6c3572e7fd9f314981f81a1e90db95
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2022-03-22 08:30:24 +0100

    add error to hero image quote

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -4,19 +4,28 @@ import { QuoteAuthor, QuoteText, QuoteWrapper, StyledHeroImage } from './HeroIma
 
 const HeroImage = () => {
   const [quote, setQuote] = useState('');
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get('https://api.quotable.io/random');
-      setQuote(response.data);
+      try {
+        const response = await axios.get('https://api.quotable.io/random');
+        setQuote(response.data);
+      } catch (e) {
+        setIsError(true);
+      }
     })();
   }, []);
 
   return (
     <StyledHeroImage>
       <QuoteWrapper>
-        <QuoteText>{quote.content}</QuoteText>
-        <QuoteAuthor>{quote.author}</QuoteAuthor>
+        {!isError ? (
+          <>
+            <QuoteText>{quote.content}</QuoteText>
+            <QuoteAuthor>{quote.author}</QuoteAuthor>
+          </>
+        ) : null}
       </QuoteWrapper>
     </StyledHeroImage>
   );
